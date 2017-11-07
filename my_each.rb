6c3b4c68e1d60610817c6891f6b9f97733753b8f
Sherry <sherryptk@gmail.com>
2017-11-07 23:46:17 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -3,16 +3,14 @@ def my_each (array)# put argument(s) here
   if block_given?
 
   i=0
-  while i<array.length
-    yield (array[i])
-    i = i + 1
-  end
+    while i<array.length
+      yield (array[i])
+      i = i + 1
+    end
   array
-else
-   "This block should not run!"
-end
+  else
+     "This block should not run!"
+  end
+  
 end
 
-# def hello(['arel', 'jon', 'logan', 'spencer']) do |name|
-#     puts "Hi, #{name}"
-# end
